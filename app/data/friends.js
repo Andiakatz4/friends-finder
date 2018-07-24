4708b72def3a9eebf44a9fc55f22cb1b0c5abacd
Andia <andiakatz@gmail.com>
2018-07-23 22:55:52 -0700

    code for submit buttons

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,165 @@
-// var express = require("express");
-// var bodyParser = require("body-parser");
-// var path = require("path");
+// Create an array to store friends scores
+var friendsArr = [
+    {
+        "name": 'Frankie',
+        "photo": 'https://assets3.thrillist.com/v1/image/2754967/size/tmg-article_tall;jpeg_quality=20.jpg',
+        "scores": [
+            '5',
+            '1',
+            '4',
+            '5',
+            '1',
+            '2',
+            '5',
+            '4',
+            '1',
+            '1'
+        ]
+    },
+    {
+        "name": 'Newton',
+        "photo": 'https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&h=350',
+        "scores": [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        "name": 'Fluff',
+        "photo": 'http://4.bp.blogspot.com/-zJcUIVbTbLY/UgQb716mJoI/AAAAAAAAEvw/lw2vTw_7zhY/s1600/41+rock+bottom+2.jpeg',
+        "scores": [
+            '4',
+            '3',
+            '5',
+            '1',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+    {
+        "name": 'Whitey',
+        "photo": 'https://i.ytimg.com/vi/cEJy2q27hVk/maxresdefault.jpg',
+        "scores": [
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3'
+        ]
+    },
+    {
+        "name": 'Cheap',
+        "photo": 'https://www.lilblueboo.com/wp-content/uploads/2016/08/mottledcochin.jpg',
+        "scores": [
+            '1',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '1'
+        ]
+    },
+    {
+        "name": 'Flowers',
+        "photo": 'https://data.whicdn.com/images/254065346/large.jpg',
+        "scores": [
+            '1',
+            '1',
+            '5',
+            '1',
+            '3',
+            '2',
+            '5',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        "name": 'Porky',
+        "photo": 'https://pbs.twimg.com/profile_images/796725244346122240/Ywc8vDOB_400x400.jpg',
+        "scores": [
+            '5',
+            '4',
+            '4',
+            '2',
+            '4',
+            '4',
+            '3',
+            '2',
+            '3',
+            '3'
+        ]
+    },
+    {
+        "name": 'Grass',
+        "photo": 'http://www.cutestpaw.com/wp-content/uploads/2015/05/zxBaby-Pony-3-Wochen-alt-a195164861.jpg',
+        "scores": [
+            '5',
+            '2',
+            '3',
+            '1',
+            '5',
+            '1',
+            '3',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        "name": 'Chop',
+        "photo": 'https://i.redd.it/qwovdoo3a3ay.jpg',
+        "scores": [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        "name": 'Quack',
+        "photo": 'https://i.pinimg.com/736x/ec/63/b0/ec63b01ded18b2d70b285efc50d5b86a--duckface-duck-duck.jpg',
+        "scores": [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ]
+    }
+];
 
-// // json
-// // {
-// //   "name":"Ahmed",
-// //   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-// //   "scores":[
-// //       5,
-// //       1,
-// //       4,
-// //       4,
-// //       5,
-// //       1,
-// //       2,
-// //       5,
-// //       4,
-// //       1
-// //     ]
-// // }
+module.exports = friendsArr;
